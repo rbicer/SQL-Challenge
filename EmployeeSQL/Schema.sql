657db3aea3c99c71a66e6c680e9da1a6f6d516d5
R Bicer <rsn.bicer@hotmail.com>
2021-02-02 09:12:57 -0500

    odev

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -11,6 +11,8 @@ SELECT * FROM departments
 CREATE TABLE dept_manager (
     dept_no VARCHAR   NOT NULL,
     emp_no INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no,dept_no));
 
 --Verify dept_manager data
@@ -20,6 +22,8 @@ SELECT * FROM dept_manager
 CREATE TABLE dept_emp (
 	emp_no INT   NOT NULL,
     dept_no VARCHAR   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (dept_no,emp_no));
 
 --Verify dept_emp data
@@ -34,6 +38,7 @@ CREATE TABLE employees (
     last_name VARCHAR   NOT NULL,
     sex VARCHAR   NOT NULL,
     hire_date DATE   NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
     PRIMARY KEY (emp_no));
 
 --Verify employees data	
@@ -52,6 +57,7 @@ SELECT * FROM titles
 CREATE TABLE salaries (
     emp_no INT   NOT NULL,
     salary INT   NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no));
 
 --Verify salaries data
