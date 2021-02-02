4b71279b77ccc7a9d79528e891de419a2f321499
R Bicer <rsn.bicer@hotmail.com>
2021-02-02 09:21:01 -0500

    odev

--- EmployeeSQL/Schema.sql
+++ EmployeeSQL/Schema.sql
@@ -1,7 +1,7 @@
 -- Create departments table and import csv files
 CREATE TABLE departments (
-    dept_no VARCHAR   NOT NULL,
-    dept_name VARCHAR   NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
+    dept_name VARCHAR(40)   NOT NULL,
     PRIMARY KEY (dept_no));
 
 --Verify departments data
@@ -9,7 +9,7 @@ SELECT * FROM departments
 
 -- Create dept_manager table and import csv files
 CREATE TABLE dept_manager (
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
     emp_no INT   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
@@ -21,7 +21,7 @@ SELECT * FROM dept_manager
 -- Create dept_emp table and import csv files
 CREATE TABLE dept_emp (
 	emp_no INT   NOT NULL,
-    dept_no VARCHAR   NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (dept_no,emp_no));
@@ -32,11 +32,11 @@ SELECT * FROM dept_emp
 -- Create employees table and import csv files
 CREATE TABLE employees (
     emp_no INT   NOT NULL,
-    emp_title_id VARCHAR   NOT NULL,
+    emp_title_id VARCHAR(10)   NOT NULL,
     birth_date DATE   NOT NULL,
-    first_name VARCHAR   NOT NULL,
-    last_name VARCHAR   NOT NULL,
-    sex VARCHAR   NOT NULL,
+    first_name VARCHAR (20)  NOT NULL,
+    last_name VARCHAR  (20) NOT NULL,
+    sex VARCHAR (1)  NOT NULL,
     hire_date DATE   NOT NULL,
     FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
     PRIMARY KEY (emp_no));
@@ -46,8 +46,8 @@ SELECT * FROM employees
 
 -- Create titles table and import csv files
 CREATE TABLE titles (
-    title_id VARCHAR   NOT NULL,
-    title VARCHAR   NOT NULL,
+    title_id VARCHAR (10)  NOT NULL,
+    title VARCHAR (30)  NOT NULL,
     PRIMARY KEY (title_id));
 
 --Verify titles data
